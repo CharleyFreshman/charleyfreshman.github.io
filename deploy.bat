d82590c0b21b0ae2c350f631413079dfb3cf9b12
Sifan Song <sifansong432@qq.com>
2026-04-29 15:11:00 +0800

    Update deploy script and fix CV link

--- deploy.bat
+++ deploy.bat
@@ -1,7 +1,13 @@
 @echo off
-REM 部署脚本 - 绕过PowerShell特殊字符限制
+REM 部署脚本 - 将学术主页推送到GitHub Pages
 
+cd /d "D:\Trae CN\Homepage"
+
+REM 添加所有修改到暂存区
 git add .
-git commit -m "Initial commit - personal homepage"
-git remote add origin https://github.com/CharleyFreshman/Garth.git
-git push -u origin main
+
+REM 提交更改
+git commit -m "Update academic homepage with bilingual support and CV"
+
+REM 推送到GitHub Pages仓库的main分支
+git push -u origin master:main
